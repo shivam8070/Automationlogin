518d9257fca46b1ff180c36b31da02414e3af669
Shivam <shivamvats141@gmail.com>
2024-12-07 13:44:07 +0530

    shivamandmohnishbhaifluttervale

---
+++ Arrays_and_funaction.java
@@ -0,0 +1,26 @@
+package corejava;
+
+public class Arrays_and_funaction {
+	public static void update (int marks[]) {
+		
+		for (int i=0; i<marks.length ; i++) {
+			marks[i]=marks[i]+1;
+			
+		}
+		
+	}
+	
+public static void main(String[] args) {
+	
+	
+	int marks[]	= {97,98,99};
+	update (marks);
+	
+	for (int i=0; i<marks.length ; i++) {
+		System.out.println(marks[i]+"");
+		
+	}
+	System.out.println( );
+	
+	}
+}
